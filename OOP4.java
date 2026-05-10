9ea95c4121f7cf8528fb80a37d9b19f17e682af0
Md Ahsan Habib <habibahsan9503@gmail.com>
2026-05-10 12:36:59 +0600

    Type of Inheritance. 1
    Added classes for single level inheritance example with area method.

---
+++ OOP4.java
@@ -0,0 +1,32 @@
+/*Type of Inheritance
+    1. Single level inheritance
+
+            Base Class
+                ⬇️
+                ⬇️
+                ⬇️
+            Derived Class
+                
+     */
+
+class Shape{
+    public void area(){  // base class
+        System.out.println("Display area");
+    }
+}
+
+class Triangle extends Shape{ // derive class
+    public void area(int l, int h){
+        System.out.println(0.5*l*h);
+    }
+}
+
+
+public class OOP4 {
+    public static void main(String args[]){
+        Triangle t1 = new Triangle();
+
+        t1.area();
+        t1.area(10, 20);   // direct input
+    }
+}
